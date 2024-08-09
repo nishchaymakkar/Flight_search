d653a2ac18f88007ab019aba0df1a8e3568629fc
21Neutron <mayankbisht2113@gmail.com>
2024-08-09 21:10:57 +0530

    fixed

--- app/src/main/java/com/example/flightsearchapp/data/Airports.kt
+++ app/src/main/java/com/example/flightsearchapp/data/Airports.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "airports")
+@Entity(tableName = "airport")
 data class Airports(
     @PrimaryKey
     val id: Int = 0,
